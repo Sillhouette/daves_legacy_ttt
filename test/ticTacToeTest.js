73a59c9c38b41bc6ca5c0c68f4b71cf485a6b175
Austin Melchior <raustin.melchior@gmail.com>
2020-03-30 13:43:36 -0700

    add another dummy test to test CI

--- test/ticTacToeTest.js
+++ test/ticTacToeTest.js
@@ -3,4 +3,8 @@ describe("Dummy Test", function() {
       expect(true).to.equal(true);
     });
 })
-  
+describe("Dummy Test", function() {
+    it( "is a dummy test", function() {
+        expect(true).to.equal(true);
+      });
+  })

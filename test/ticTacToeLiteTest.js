20c6df753b29b4e9644ba4934504ff2c04c7d25c
Austin Melchior <raustin.melchior@gmail.com>
2020-04-08 17:37:56 -0700

    add ticTacToeLite feature unit tests, remove outdated tests

--- test/ticTacToeLiteTest.js
+++ test/ticTacToeLiteTest.js
@@ -2,76 +2,62 @@ let tttLite = require('../ticTacToeLite.js')
 let helpers = require('./unitTestHelpers')
 
 describe("ticTacToeLite.js", function() {
-
-  describe("#resetPosition", function() {
-    this.beforeEach(function(){
-      ttt.resetState()
-    })
-
-    it( "removes a player indicator from the state of the board", function() {
-        let index = 1
-        let playerIndicator = 2
-        expected = 0
-
-        ttt.changeState(index, playerIndicator)
-        tttLite.resetPosition(index)
-        actual = helpers.getStateAtIndex(index)
-
-        expect(actual).equal(expected);
-    });
+  this.beforeEach(function() {
+        ttt.enableLite()
   })
 
-  describe("#clearTokenFromBoard", function() {
-      this.beforeEach(function() {
-          ttt.resetState()
-      })
+  describe("#addMoveToHistory", function() {
 
-      it("sets a position on the board back to an empty space", function() {
-        let index = 1
-        let playerToken = "X"
-        expected = "&nbsp;"
+    it("can add a move to the history", function() {
+        expected = 4
 
-        $(`td#${index}`).html(playerToken)
-        tttLite.clearTokenFromBoard(index)
-        actual = $(`td#${index}`).html()
+        tttLite.addMoveToHistory(expected)
+        actual = tttLite.getRemovalIndex()
 
-        expect(actual).equal(expected)
-      })
-
-      it("sets a different position on the board back to an empty space", function() {
-        let index = 5
-        let playerToken = "O"
-        expected = "&nbsp;"
-
-        $(`td#${index}`).html(playerToken)
-        tttLite.clearTokenFromBoard(index)
-        actual = $(`td#${index}`).html()
-
-        expect(actual).equal(expected)
-      })
-  })
-
-  describe("#executeTokenRemoval", function() {
-    this.beforeEach(function() {
-        ttt.resetState()
+        expect(actual).to.equal(expected)
     })
 
-    it("removes a token from the board and resets a state position when given an index", function() {
-        let index = 8
-        let playerIndicator = 2
-        let playerToken = "X"
-        expectedBoardValue = "&nbsp;"
-        expectedStateValue = 0
+    it("can add multiple moves to the history", function() {
+        moves = [4, 6, 7, 2, 1]
 
-        ttt.changeState(index, playerIndicator)
-        $(`td#${index}`).html(playerToken)
-        tttLite.executeTokenRemoval(index)
-        
-        actualStateValue = helpers.getStateAtIndex(index)
-        actualBoardValue = $(`td#${index}`).html()
+        moves.forEach(function(move){
+            tttLite.addMoveToHistory(move)
+        })
 
-        expect(actualStateValue).to.equal(expectedStateValue)
-        expect(actualBoardValue).to.equal(expectedBoardValue)
+        moves.forEach(function(move, index){
+            expected = moves[index]
+            actual = tttLite.getRemovalIndex()
+
+            expect(actual).to.equal(expected)
+        })
+    })
+
+  })
+
+  describe("#getRemovalIndex", function() {
+
+    it("returns the first token added to the queue", function() {
+        expected = 4
+
+        tttLite.addMoveToHistory(expected)
+        actual = tttLite.getRemovalIndex()
+
+        expect(actual).to.equal(expected)
+    })
+
+    it("returns moves in the order that they were added", function() {
+        moves = [4, 6, 7, 2, 1]
+
+        moves.forEach(function(move){
+            tttLite.addMoveToHistory(move)
+        })
+
+        moves.forEach(function(move, index){
+            expected = moves[index]
+            actual = tttLite.getRemovalIndex()
+
+            expect(actual).to.equal(expected)
+        })
     })
 
   })
